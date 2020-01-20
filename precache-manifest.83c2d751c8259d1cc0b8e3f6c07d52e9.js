add25281ba8eb7fb74b17533d03e2506c7e850dc
vadimkaKharitonenko <vadimka.kharitonenk@mail.ru>
2020-01-20 21:56:03 +0700

    Updates

---
+++ precache-manifest.83c2d751c8259d1cc0b8e3f6c07d52e9.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1dedbc9ea0c9de04631fcf9b33626e5",
+    "url": "/fltest/index.html"
+  },
+  {
+    "revision": "44331f13b79947d79faa",
+    "url": "/fltest/static/css/main.9b34b25a.chunk.css"
+  },
+  {
+    "revision": "df1d8d88a80c07bee8a7",
+    "url": "/fltest/static/js/2.da2080b5.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/fltest/static/js/2.da2080b5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "44331f13b79947d79faa",
+    "url": "/fltest/static/js/main.85b7c8da.chunk.js"
+  },
+  {
+    "revision": "2cd42da0fcc431be56ec",
+    "url": "/fltest/static/js/runtime-main.1d4da0ae.js"
+  },
+  {
+    "revision": "283174e2410939a3360d41f9a33be0da",
+    "url": "/fltest/static/media/Mint Type - Pancetta Pro Regular.283174e2.eot"
+  },
+  {
+    "revision": "36d5871fc8ade3838c0a0295248a6ce9",
+    "url": "/fltest/static/media/Mint Type - Pancetta Pro Regular.36d5871f.woff"
+  },
+  {
+    "revision": "64cfb130db68114b4173e3922b5a53cc",
+    "url": "/fltest/static/media/Mint Type - Pancetta Pro Regular.64cfb130.svg"
+  },
+  {
+    "revision": "8ed85b27438bae366557335484ba0e5a",
+    "url": "/fltest/static/media/Mint Type - Pancetta Pro Regular.8ed85b27.ttf"
+  },
+  {
+    "revision": "3dc9023ccfcf1ec96482606bcbe902ee",
+    "url": "/fltest/static/media/SourceSansPro-Regular.3dc9023c.eot"
+  },
+  {
+    "revision": "565f8997dbd98749674d187b8ab65ce2",
+    "url": "/fltest/static/media/SourceSansPro-Regular.565f8997.woff"
+  },
+  {
+    "revision": "9ba6f592e924057c6e9746649e9a15aa",
+    "url": "/fltest/static/media/SourceSansPro-Regular.9ba6f592.svg"
+  },
+  {
+    "revision": "d1f424dd311430ad4faba85f6fe53268",
+    "url": "/fltest/static/media/SourceSansPro-Regular.d1f424dd.ttf"
+  },
+  {
+    "revision": "defc2f1b81c51ae75aba4b918a3af7ce",
+    "url": "/fltest/static/media/card-bg.defc2f1b.jpg"
+  }
+]);
